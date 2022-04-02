36a0f152d8b3f2ca00e6a8c3cdf47db5fa2195bf
priyodas1997 <priyo.honey.1997@gmail.com>
2022-04-02 18:26:54 +0530

    updated

---
+++ payment.js
@@ -0,0 +1,32 @@
+var payment=JSON.parse(localStorage.getItem("address"));
+console.log("payment");
+payment.map(function (elem){
+
+    var addDiv = document.querySelector(".bigthree");
+
+    var p1=document.createElement("p");
+   p1.innerText=elem.firstname;
+
+
+var p2=document.createElement("p");
+p2.innerText=elem.mobile;
+
+var p3=document.createElement("p");
+p3.innerText=elem.country;
+
+var p4=document.createElement("p");
+p4.innerText=elem.pincode;
+
+var p5=document.createElement("p");
+p5.innerText=elem.address;
+
+var p6=document.createElement("p");
+p6.innerText=elem.city;
+
+var p7=document.createElement("p");
+p7.innerText=elem.state;
+
+addDiv.append(p1,p2,p3,p4,p5,p6,p7);
+
+
+});
