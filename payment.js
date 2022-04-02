9c7849480089c46d96be0a4738f86dcf7338a6c9
Rohit24-code <rohitsingh17031995@gmail.com>
2022-04-02 21:47:37 +0530

    payment side

--- payment.js
+++ payment.js
@@ -1,32 +1,34 @@
-var payment=JSON.parse(localStorage.getItem("address"));
-console.log("payment");
-payment.map(function (elem){
+var payment = JSON.parse(localStorage.getItem("address")) ;
+// console.log("payment");
+payment.map(function (elem) {
 
     var addDiv = document.querySelector(".bigthree");
+    var newdiv = document.querySelector(".bigthreefirst")
 
-    var p1=document.createElement("p");
-   p1.innerText=elem.firstname;
+    var p1 = document.createElement("p");
+    p1.innerText = elem.firstname;
 
 
-var p2=document.createElement("p");
-p2.innerText=elem.mobile;
+    var p2 = document.createElement("p");
+    p2.innerText = elem.mobile;
 
-var p3=document.createElement("p");
-p3.innerText=elem.country;
+    var p3 = document.createElement("p");
+    p3.innerText = elem.country;
 
-var p4=document.createElement("p");
-p4.innerText=elem.pincode;
+    var p4 = document.createElement("p");
+    p4.innerText = elem.pincode;
 
-var p5=document.createElement("p");
-p5.innerText=elem.address;
+    var p5 = document.createElement("p");
+    p5.innerText = elem.address;
 
-var p6=document.createElement("p");
-p6.innerText=elem.city;
+    var p6 = document.createElement("p");
+    p6.innerText = elem.city;
 
-var p7=document.createElement("p");
-p7.innerText=elem.state;
+    var p7 = document.createElement("p");
+    p7.innerText = elem.state
+    newdiv.append(p1, p2, p3, p4, p5)
 
-addDiv.append(p1,p2,p3,p4,p5,p6,p7);
 
+    addDiv.append(newdiv);
 
 });
